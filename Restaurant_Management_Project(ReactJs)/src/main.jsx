0895e5fa8c51c9fb5bfc8efb64e16c462f596d23
DipItlogiko <dipitlogiko@gmail.com>
2023-12-03 09:31:10 +0600

    ReactProject setup done

---
+++ Restaurant_Management_Project(ReactJs)/src/main.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client' 
+
+const root = ReactDOM.createRoot(document.getElementById('root')); 
+
+root.render(
+  <div>
+    <h1>Restauran Management Project Clint Side</h1>
+    <h2>Welcome</h2>
+  </div>
+);
