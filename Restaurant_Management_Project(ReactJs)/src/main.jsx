dfb0409c2fcd9e1ba59927166e2fcefa56a20ae9
DipItlogiko <dipitlogiko@gmail.com>
2023-12-03 23:28:34 +0600

    Transfering Blade view to ReactJs

--- Restaurant_Management_Project(ReactJs)/src/main.jsx
+++ Restaurant_Management_Project(ReactJs)/src/main.jsx
@@ -1,11 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client' 
+import App from './App'
 
 const root = ReactDOM.createRoot(document.getElementById('root')); 
 
 root.render(
-  <div>
-    <h1>Restauran Management Project Clint Side</h1>
-    <h2>Welcome</h2>
-  </div>
+   <App />
 );
